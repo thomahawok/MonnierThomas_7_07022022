07cb30bd4718dd3cb5044a6bd476c32cd5c8d7d2
Thomas Monnier <thomahawok0@gmail.com>
2022-03-15 14:49:13 +0100

    loop for Ok in filer script

--- scripts/utils/filterElementsLists.js
+++ scripts/utils/filterElementsLists.js
@@ -1,41 +1,52 @@
 /* eslint-disable no-unused-vars */
 /*************/
 function getIngredients(filtredRecipes, tagsItemIngredients) {
-  console.log(tagsItemIngredients)
   const ingredients = [] /*Tableau de l'ensembre des ingrédient*/
-  Object.keys(filtredRecipes).forEach((key) => {
-    let recipeIng = filtredRecipes[key].ingredients
-    Object.keys(recipeIng).forEach((key) => {
-      let recipeIngIng = recipeIng[key].ingredient
-      ingredients.(recipeIngIng.toLowerCase())
-    })
-  })
 
+  for (let i = 0; i < filtredRecipes.length; i++) {
+    for (let j = 0; j < filtredRecipes[i].ingredients.length; j++) {
+      let recipeIngIng = filtredRecipes[i].ingredients[j].ingredient
+      ingredients.push(recipeIngIng.toLowerCase())
+    }
+  }
   return filterElementList(ingredients, tagsItemIngredients)
 }
 
 function getAppliances(filtredRecipes, tagsItemAppilances) {
   const appliances = [] /*Tableau de l'ensembre des ingrédient*/
+  for (let i = 0; i < filtredRecipes.length; i++) {
+    let recipeAppliance = filtredRecipes[i].appliance
+    appliances.push(recipeAppliance.toLowerCase())
+  }
+  /*
   Object.keys(filtredRecipes).forEach((key) => {
     let recipeAppliance = filtredRecipes[key].appliance
     appliances.push(recipeAppliance.toLowerCase())
-  })
+  })*/
   return filterElementList(appliances, tagsItemAppilances)
 }
 
 function getUstensils(filtredRecipes, tagsItemUstenceils) {
   let ustensils = [] /*Tableau de l'ensembre des ingrédient*/
 
+  for (let i = 0; i < filtredRecipes.length; i++) {
+    for (let j = 0; j < filtredRecipes[i].ustensils.length; j++) {
+      let ustensilList = filtredRecipes[i].ustensils[j]
+      ustensils.push(ustensilList.toLowerCase())
+    }
+  }
+
+  /*
   filtredRecipes.forEach((recipe) =>
     recipe.ustensils.forEach((ustensil) =>
       ustensils.push(ustensil.toLowerCase())
     )
   )
+  */
   return filterElementList(ustensils, tagsItemUstenceils)
 }
 
 function filterElementList(getedElements, anyArrayTags) {
-  console.log(anyArrayTags)
   const filteredElement = getedElements.filter(onlyUniqueInliste)
   filteredElement.sort((a, b) => a.localeCompare(b))
   if (anyArrayTags) {
